1bbf43149fcd8065e6210dd5272530afdf521d43
Vishal Singh Thakur <vishalsingh63649@gmail.com>
2024-02-10 19:41:03 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+     <meta charset="urf-8">
+	 <meta name="viewport" content="width=device=width, initial-scale=1">
+     <title>Php Crud Operation</title>
+     <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+    <div class="container">
+    <div class="title">
+       Registration Form
+    </div>
+        <div class="form">
+        <div class="input_field">
+          <label>First Name</label>
+          <input type="text" class="input"/>
+        </div>
+		
+        <div class="input_field">
+          <label>Last Name</label>
+          <input type="text" class="input"/>
+		</div>
+		
+        <div class="input_field">
+         <label>Password</label>
+         <input type="password" class="input"/>
+        </div>
+		
+        <div class="input_field">
+         <label>Confirm Password</label>
+         <input type="password" class="input"/>
+        </div>
+		
+        <div class="input_field">
+         <label>Gender</label>
+		 <div class="selectbox">
+         <select>
+		 <option>Select</option>
+		 <option>Male</option>
+		 <option>Female</option>
+		 <option>Other</option>
+		 </select>
+         </div>
+		 </div>
+		 
+        <div class="input_field">
+         <label>Email Address</label>
+         <input type="text" class="input"/>
+        </div>
+		
+        <div class="input_field">
+         <label>Phone Number</label>
+         <input type="number" class="input"/>
+        </div>
+		
+        <div class="input_field">
+         <label>Address</label>
+         <textarea class="textarea"></textarea>
+        </div>
+		
+		<div class="input_field terms">
+         <label class="check">
+		 <input type="checkbox"/>
+		 <span class="checkmark"></span>
+		 </label>
+		 <p>Agree to terms and conditions</p>
+		</div>
+		
+		<div class="input_field">
+		<input type="submit" value="Register" class="btn"/>
+		</div>
+	</div>	
+		
+</div>
+
+
+
+
+</body>
+
+</html>
+
+
+
